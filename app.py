6634beaa9ea44770aae6c6e8667e1db7c473da79
Somu6241 <137025571+Somu6241@users.noreply.github.com>
2023-07-23 16:49:10 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+# Create User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.first_name} {self.last_name}>"
+
+@app.route('/')
+def home():
+    return redirect(url_for('signin'))
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email, password=password).first()
+
+        if user:
+            return redirect(url_for('secret_page'))
+        else:
+            return "Invalid credentials. Please try again."
+
+    return render_template('signin.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # Get form data from request
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        # Check if passwords match and they meet criteria
+        if password != confirm_password:
+            return "Passwords do not match. Please try again."
+
+        # Check if the email is not already used in the database
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            return "Email address already used. Please try a different email."
+
+        # If everything is okay, save the user data to the database
+        new_user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template('thankyou.html')
+
+    return render_template('signup.html')
+
+@app.route('/secretpage')
+def secret_page():
+    # Implement code to check if the user is logged in
+    # For simplicity, we will assume that the user is logged in if they reach this page.
+    return render_template('secretPage.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port= 5001)
